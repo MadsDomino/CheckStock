ae2c961370a05a07e05a48566ce076e59e9ea26e
Shievo <Shievo@LAPTOP-Q36NKBH1>
2016-11-15 10:17:31 +0100

    Add project files.

---
+++ ClassLibrary1/ItemRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _CheckStock
+{
+    interface IItemRepository
+    {
+        void DeleteWareByID(int ID);
+        void UpdateWare(Iware ware);
+        List<Iware> getWares();
+        Iware GetWareByID(int ID);
+    }
+
+    public class ItemRepository : IItemRepository
+    {
+        private List<Iware> wareList;
+
+        public void DeleteWareByID(int ID)
+        {
+            foreach(Iware item in wareList)
+            {
+                if (item.GetID() == ID)
+                {
+                    wareList.Remove(item);
+                }
+            }
+        }
+
+        public Iware GetWareByID(int ID)
+        {
+            foreach(Iware item in wareList)
+            {
+                if (item.GetID() == ID)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public List<Iware> getWares()
+        {
+            return wareList;
+        }
+
+        public void UpdateWare(Iware ware)
+        {
+            foreach(Iware item in wareList)
+            {
+                if(item.GetID() == ware.GetID())
+                {
+                    wareList.Remove(item);
+                    wareList.Add(ware);
+                }
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/ware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _CheckStock
+{
+    public interface Iware
+    {
+        int GetID();
+        int GetPrice();
+        string GetDesignation();
+    }
+    public class ware : Iware
+    {
+        public int ID { get; set; }
+        public int Price { get; set; }
+        public string Designation { get; set; }
+
+        public ware()
+        {
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+
+        public int GetPrice()
+        {
+            return Price;
+        }
+
+        public string GetDesignation()
+        {
+            return Designation;
+        }
+    }
+}
